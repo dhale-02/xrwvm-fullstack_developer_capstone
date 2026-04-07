83448da1dbddbe64bf86192727ad4c032fb7322e
dhale-02 <dhale.caparros@caparrosui.com>
2026-04-07 18:34:48 +0800

    Add frontend React routes to Django URLs

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls', namespace='djangoapp')),
+    # React frontend routes
+    path('dealers', TemplateView.as_view(template_name='frontend/index.html')),
+    re_path(r'^dealer/.*$', TemplateView.as_view(template_name='frontend/index.html')),
+    re_path(r'^postreview/.*$', TemplateView.as_view(template_name='frontend/index.html')),
+    path('login', TemplateView.as_view(template_name='frontend/index.html')),
+    path('register', TemplateView.as_view(template_name='frontend/index.html')),
     path('', TemplateView.as_view(template_name='index.html')),
 ]
